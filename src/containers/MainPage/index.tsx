c5bc720d29d2492c47de884627de4a73f7d6d3aa
Behnam Gholipour <behnam.gholipoor@gmail.com>
2024-06-10 22:22:42 +0330

    change MainPage container and add new todo section

--- src/containers/MainPage/index.tsx
+++ src/containers/MainPage/index.tsx
@@ -1,9 +1,15 @@
+import { Button, Card, TextInput } from "@/components/UI"
 import { FC } from "react"
 
 const MainPage:FC = () => {
   return (
     <section className="w-full max-w-[300px] md:max-w-[600px] inset-0 m-auto">
-      <h2 className="text-white">Main Section</h2>
+      <Card className="mt-12" hasLabel label="Add New Todo">
+        <div className="flex justify-between items-center gap-2">
+          <TextInput id="todoInput" hasLabel={false} />
+          <Button label="Add" className="bg-sp-blue text-white" />
+        </div>
+      </Card>
     </section>
   )
 }
